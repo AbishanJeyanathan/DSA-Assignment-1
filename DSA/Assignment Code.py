b86d33545665d3ccb82ddca36ac464561970eb16
AbishanJeyanathan <74685751+AbishanJeyanathan@users.noreply.github.com>
2024-02-17 12:06:17 -0500

    Add files via upload

---
+++ DSA/Assignment Code.py
@@ -0,0 +1,11 @@
+class Product:
+    def __init__(self, product_id, name, price, category):
+        self.product_id = product_id
+        self.name = name
+        self.price = price
+        self.category = category
+
+def load_product_data(file_path):
+    products = []
+ 
+
