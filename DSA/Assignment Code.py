680f89e463fbd5d05d71060621c8625fca70bf1a
AbishanJeyanathan <74685751+AbishanJeyanathan@users.noreply.github.com>
2024-02-18 20:42:04 -0500

    update

--- DSA/Assignment Code.py
+++ DSA/Assignment Code.py
@@ -20,17 +20,29 @@ def show_items(items):
     for item in items:
         print("Item ID:", item.item_id, "Name:", item.name, "Price:", item.price, "Category:", item.category)
 
-def bubble_sort(items):
+# Bubble Sort for Least to Greastest
+def bubble_sort_1(items):
     n = len(items)
     for c in range(n-1):
         for a in range(0, n-c-1):
             if items[a].price > items[a+1].price:
-                items[a], items[a+1] = items[a+1], items[a]           
+                items[a], items[a+1] = items[a+1], items[a]                    
     return items     
 
+# Bubble Sort for Greatest to Least
+def  bubble_sort_2(items):
+    n = len(items)
+    for c in range(n-1):
+        for a in range(0, n-c-1):       
+            if items[a].price < items[a+1].price:
+                items[a], items[a+1] = items[a+1], items[a]               
+    return items     
+
+# Inserts Item
 def insert_item(items, new_item):
     items.append(new_item)
 
+# Delete Item
 def delete_item(items, item_id):
     for i, item in enumerate(items):
         if item.item_id == item_id:
@@ -38,6 +50,7 @@ def delete_item(items, item_id):
             return True
     return False
 
+# Update Item
 def update_item(items, updated_item):
     for i, item in enumerate(items):
         if item.item_id == updated_item.item_id:
@@ -45,12 +58,14 @@ def update_item(items, updated_item):
             return True
     return False
 
+# Search Item
 def search_item_by_id(items, item_id):
     for item in items:
         if item.item_id == item_id:
             return item
     return None
 
+# Main Function
 def main():
     file = "product_data.txt"
     items = load_item_data(file)
@@ -59,12 +74,25 @@ def main():
     print("\nCurrent Item Data:")
     show_items(item_data)
     
+    print("\nItem Data Sorted By Price From Least To Greatest:")
+    sorted_items = item_data
     starttime = time.time()
-    sorted_items = bubble_sort(item_data)
+    sorted_items = bubble_sort_1(item_data)
     endtime = time.time()
     print("Time Taken: ", endtime-starttime)
-    print("\nItem Data Sorted By Price:")
+
     show_items(sorted_items)
+   
+    print("\nItem Data Sorted By Price From Greatest To Least:")
+    print("\nSorting reverse sorted data:")
+    reverse_sorted_items = item_data
+    starttime = time.time()
+    sorted_reverse_items = bubble_sort_2(reverse_sorted_items)
+    endtime = time.time()
+    print("Time Taken: ", endtime-starttime)
+
+    show_items(sorted_items)
+
 
     # Creating a new item object and inserting it
     new_item = Item(67879, 'New Product', 67.89, 'Home & Kitchen')
